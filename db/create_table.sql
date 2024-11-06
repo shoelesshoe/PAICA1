30ab3ce337df905082330895fe4a7bc918d3c9e0
limbby <junyi0616c@gmail.com>
2024-11-06 13:01:23 +0800

    edited sql customer table

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,29 +1,30 @@
 use PAI_CA1_test
 
 CREATE TABLE customer (
-    country_code VARCHAR(10),                -- Assuming country codes are short strings
-    customer_id VARCHAR(10) PRIMARY KEY,              -- Assuming customer IDs are integers
-    mobile_verified INT,                      -- Boolean value (1 for true, 0 for false)
-    num_orders_last_50days INT,              -- Number of orders in the last 50 days
-    num_cancelled_orders_last_50days INT,    -- Number of cancelled orders in the last 50 days
-    num_refund_orders_last_50days INT,       -- Number of refund orders in the last 50 days
-    total_payment_last_50days DECIMAL(18, 2), -- Total payment as a decimal (for currency)
-    num_associated_customers INT,             -- Number of associated customers
-    first_order_datetime DATETIME             -- Date and time of the first order
+    country_code VARCHAR(10),                
+    customer_id VARCHAR(50),                  
+    mobile_verified BIT,                      
+    num_orders_last_50days INT,               
+    num_cancelled_orders_last_50days INT,     
+    num_refund_orders_last_50days INT,        
+    total_payment_last_50days DECIMAL(18, 2), 
+    num_associated_customers INT,             
+    first_order_datetime DATETIME,            
+    PRIMARY KEY (country_code, customer_id)   
 );
 
 
 use PAI_CA1_test
 
 BULK INSERT customer
-FROM 'E:\SP\Year 2 Sem 2\ST1508 Practical AI\PAICA1\datasets\final_customer.csv'
+FROM 'C:\Users\p2308924\Practical AI (PAI)\Github CA\PAICA1\Datasets\final_customer.csv'
 WITH (
-    FIELDTERMINATOR = ',',  -- Change to your field delimiter if necessary
-    ROWTERMINATOR = '\n',   -- Change if your file uses a different row terminator
-    FIRSTROW = 2            -- Set to 2 if your file has a header row
+    FIELDTERMINATOR = ',', 
+    ROWTERMINATOR = '\n',   
+    FIRSTROW = 2            
 );
 
-use Practical_AI_sk
+use PAI_CA1_test
 
 CREATE TABLE orders (
 	country_code CHAR(2),                
@@ -38,10 +39,10 @@ CREATE TABLE orders (
 );
 
 
-use Practical_AI_sk
+use PAI_CA1_test
 
 BULK INSERT orders
-FROM 'C:\Users\P2300533\Documents\PAI\PAICA1\datasets\final_order.csv'
+FROM 'C:\Users\p2308924\Practical AI (PAI)\Github CA\PAICA1\Datasets\final_order.csv'
 WITH (
     FIELDTERMINATOR = ',',  
     ROWTERMINATOR = '\n',   
