6aba52e880d50ba97c0d967e63a0a7150bbcadc2
limb <junyi0616c@gmail.com>
2024-11-20 14:14:32 +0800

    added second query

--- db/queries.sql
+++ db/queries.sql
@@ -27,3 +27,24 @@ GROUP BY
     o.payment_method
 ORDER BY 
     fraud_percentage DESC;
+
+
+USE PAICA1 -- Top 5 Highest Spenders (Customer)
+
+SELECT TOP 5 
+    c.country_code,
+    c.customer_id,
+    SUM(o.order_value) AS total_spent,
+    COUNT(o.order_id) AS total_orders
+FROM 
+    customer c
+INNER JOIN label l
+    ON c.country_code = l.country_code 
+    AND c.customer_id = l.customer_id
+INNER JOIN [order] o
+    ON l.country_code = o.country_code 
+    AND l.order_id = o.order_id
+GROUP BY 
+    c.country_code, c.customer_id
+ORDER BY 
+    total_spent DESC;
