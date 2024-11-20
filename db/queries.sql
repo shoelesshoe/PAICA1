914dfd5d6b68c01e26907cebf6300e042e2094d1
limb <junyi0616c@gmail.com>
2024-11-20 14:09:26 +0800

    added one sql query

--- db/queries.sql
+++ db/queries.sql
@@ -9,3 +9,24 @@ INNER JOIN customer c
 INNER JOIN [order] o
     ON l.country_code = o.country_code
     AND l.order_id = o.order_id;
+
+
+USE PAICA1 -- Top 3 Payment Methods by Fraud Percentage
+
+SELECT TOP 3
+    c.customer_id,
+    c.country_code,
+    COUNT(l.order_id) AS fraudulent_orders
+FROM 
+    label l
+INNER JOIN customer c
+    ON l.country_code = c.country_code 
+    AND l.customer_id = c.customer_id
+WHERE 
+    l.is_fraud = 1
+GROUP BY 
+    c.customer_id, c.country_code
+HAVING 
+    COUNT(l.order_id) > 1
+ORDER BY 
+    fraudulent_orders DESC;
