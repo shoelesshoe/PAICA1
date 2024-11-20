330718d1db940f090dc6c4c0af513554dd254188
limb <junyi0616c@gmail.com>
2024-11-20 14:11:51 +0800

    editted the fraud percentage query

--- db/queries.sql
+++ db/queries.sql
@@ -14,19 +14,16 @@ INNER JOIN [order] o
 USE PAICA1 -- Top 3 Payment Methods by Fraud Percentage
 
 SELECT TOP 3
-    c.customer_id,
-    c.country_code,
-    COUNT(l.order_id) AS fraudulent_orders
+    o.payment_method,
+    COUNT(CASE WHEN l.is_fraud = 1 THEN 1 END) AS fraud_count,
+    COUNT(*) AS total_transactions,
+    ROUND((COUNT(CASE WHEN l.is_fraud = 1 THEN 1 END) * 100.0) / COUNT(*), 2) AS fraud_percentage
 FROM 
     label l
-INNER JOIN customer c
-    ON l.country_code = c.country_code 
-    AND l.customer_id = c.customer_id
-WHERE 
-    l.is_fraud = 1
+INNER JOIN [order] o
+    ON l.country_code = o.country_code 
+    AND l.order_id = o.order_id
 GROUP BY 
-    c.customer_id, c.country_code
-HAVING 
-    COUNT(l.order_id) > 1
+    o.payment_method
 ORDER BY 
-    fraudulent_orders DESC;
+    fraud_percentage DESC;
